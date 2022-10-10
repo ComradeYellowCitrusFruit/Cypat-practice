10402f451c438adf8d4517344775fdb4fbd93544
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-10-10 09:30:11 -0400

    Added copyright info to all files
    Added some suggested GPL copyright info to all the files, using a pseudonym, Alicia Antó Valencía. From this point further, we shall add said copyright to all our files, using the following template:
    /*
    *
    *   {FILENAME AND PATH}
    *   {IF APPLICABLE} Originally from {URL FROM WHICH THE FILE WAS DERIVED}
    *   Originally written by {NAME, PSEUDONYM, OR BLANK AT THE ORIGINAL AUTHOR'S DISCRETION} - {GITHUB LINK}
    *
    *   Cyber Competition Suite - A collection of programs for cybersecurity competitions and practices
    *   Copyright (C) 2022  Alicia Antó Valencía
    *
    *   This program is free software: you can redistribute it and/or modify
    *   it under the terms of the GNU General Public License as published by
    *   the Free Software Foundation, either version 3 of the License, or
    *   (at your option) any later version.
    *
    *   This program is distributed in the hope that it will be useful,
    *   but WITHOUT ANY WARRANTY; without even the implied warranty of
    *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    *   GNU General Public License for more details.
    *
    *   You should have received a copy of the GNU General Public License
    *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
    */

--- include/crypt/bitwise.h
+++ include/crypt/bitwise.h
@@ -1,3 +1,25 @@
+/*
+*
+*   include/crypt/bitwise.h
+*   Originally written by Alicia Antó Valencía - https://github.com/ComradeYellowCitrusFruit
+*
+*   Cyber Competition Suite - A collection of programs for cybersecurity competitions and practices
+*   Copyright (C) 2022  Alicia Antó Valencía
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef CRYPT_BITWISE_OPS_H
 #define CRYPT_BITWISE_OPS_H
 
