c60cf1bafa69efde40a391516ec7ec4e739c8ca1
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-10-15 20:56:15 -0400

    Created interproc.h
    Created interproc.h, a header for interproccess communication.

---
+++ include/network/interproc.h
@@ -0,0 +1,30 @@
+/*
+*
+*   include/network/interproc.h
+*   Originally written by Alicia Antó Valencía - https://github.com/ComradeYellowCitrusFruit
+*
+*   A collection of programs for cybersecurity competitions and practices
+*   Copyright (C) 2022  Alicia Antó Valencía
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef NETWORK_INTERPROCESS_COMMS_H
+#define NETWORK_INTERPROCESS_COMMS_H
+
+/* TCP port to send packets to for interproccess communication */
+#define TCP_OUT_PORT 44252
+/* TCP port to lisen for packets for interproccess communication */
+#define TCP_IN_PORT 44253
+
+#endif
