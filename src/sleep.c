86c45eb5206f7d6ec2abce753f1c2ff4ce1a740b
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-10-18 22:39:45 -0400

    Rewrote a lot of sleep()
    Rewrote sleep() to change the way the sleep file is recorded, and removed some unnecessary code.

--- src/sleep.c
+++ src/sleep.c
@@ -1,24 +1,24 @@
 /*
- *
- *   src/sleep.c
- *   Originally written by Alicia Antó Valencía - https://github.com/ComradeYellowCitrusFruit
- *
- *   A collection of programs for cybersecurity competitions and practices
- *   Copyright (C) 2022  Alicia Antó Valencía
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+*
+*   src/sleep.c
+*   Originally written by Alicia Antó Valencía - https://github.com/ComradeYellowCitrusFruit
+*
+*   A collection of programs for cybersecurity competitions and practices
+*   Copyright (C) 2022  Alicia Antó Valencía
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -40,36 +40,36 @@ NORETURN void sleep()
     log("Sleep file opened.");
 
     /* Create a buffer for all data used in checksum */
-    uint8_t *tmpbuf = malloc(sizeof(GF_Header_t) + (gf_state.header.varCount * sizeof(uint16_t)) + sizeof(time_t));
+    uint8_t *tmpbuf = malloc(sizeof(GF_Header_t) + (gf_state.header.varCount * sizeof(var_t)));
 
     /* Collect all data for checksum */
     memcpy(tmpbuf, &(gf_state.header), sizeof(GF_Header_t));
     memcpy(tmpbuf + sizeof(GF_Header_t), gf_state.vars, gf_state.header.varCount * sizeof(uint16_t));
-    time_t tmptime;
-    time(&tmptime);
-    memcpy(tmpbuf + gf_state.header.varCount * sizeof(uint16_t) + sizeof(GF_Header_t), &tmptime, sizeof(time_t));
-
+    
     /* Calculate checksum */
     uint8_t checksum[32];
-    SHA256_M(tmpbuf, sizeof(GF_Header_t) + (gf_state.header.varCount * sizeof(uint16_t)) + sizeof(time_t), checksum);
-    log("Checksum calculated.");
+    SHA256_M(tmpbuf, sizeof(GF_Header_t) + (gf_state.header.varCount * sizeof(var_t)), checksum);
+    log("Data hash calculated.");
 
     /* Write checksum + data to file */
-    fwrite(tmpbuf, 1, sizeof(GF_Header_t) + (gf_state.header.varCount * sizeof(uint16_t)) + sizeof(time_t), fsleep);
+    fwrite(tmpbuf, sizeof(GF_Header_t) + (gf_state.header.varCount * sizeof(uint16_t)), 1, fsleep);
     fwrite(checksum, sizeof(uint8_t), 32, fsleep);
     log("Data and checksum written to sleep file.");
-    fclose(fsleep);
-    log("Sleep file closed.");
-    free(tmpbuf);
+    
+    /* Write null hash then calculate the correct value */
+    uint8_t hash[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    fwrite(hash, sizeof(uint8_t), 32, fsleep);
+    SHA256_F(fsleep, hash);
+    log("'Chaining hash' calculated.");
+
+    /* Write the 'chaining hash' in place of the null hash */
+    fseek(fsleep, -32, SEEK_END);
+    fwrite(hash, sizeof(uint8_t), 32, fsleep);
+    log("'Chaining hash' appended to the data.");
 
     /* Save score */
     fwrite(gf_state.score, sizeof(int32_t), 1, fscore);
     log("Score saved");
-
-    /* Close all files */
-    fclose(fscore);
-    fclose(scoreLog);
-    log("Score file and score log closed.");
 
     /* Kill logging */
     log("fcloseall() and "SHUTDOWNCMD" will be called soon, logging is about to be killed.");
